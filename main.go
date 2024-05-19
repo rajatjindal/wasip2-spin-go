2fb821d64ff5c024017a7e4fef22a78173f3b8d5
Rajat Jindal <rajatjindal83@gmail.com>
2024-05-19 20:14:27 +0530

    variables not working
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- main.go
+++ main.go
@@ -1,69 +1,75 @@
 package main
 
 import (
-	"io"
 	"net/http"
-	"strings"
 
 	spinhttp "github.com/fermyon/spin-go-sdk/http"
-	"github.com/fermyon/spin-go-sdk/kv"
-	"github.com/fermyon/spin-go-sdk/llm"
+	"github.com/fermyon/spin-go-sdk/variables"
 )
 
-const prompt = `<<SYS>>
-You are a bot that generates sentiment analysis responses. Respond with a single positive, negative, or neutral.
-<</SYS>>
-[INST]
-Follow the pattern of the following examples:
+// const prompt = `<<SYS>>
+// You are a bot that generates sentiment analysis responses. Respond with a single positive, negative, or neutral.
+// <</SYS>>
+// [INST]
+// Follow the pattern of the following examples:
 
-User: Hi, my name is Bob
-Bot: neutral
+// User: Hi, my name is Bob
+// Bot: neutral
 
-User: I am so happy today
-Bot: positive
+// User: I am so happy today
+// Bot: positive
 
-User: I am so sad today
-Bot: negative
-[/INST]
+// User: I am so sad today
+// Bot: negative
+// [/INST]
 
-User: {SENTENCE}`
+// User: {SENTENCE}`
 
 func init() {
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
-		nprompt := strings.ReplaceAll(prompt, "{SENTENCE}", r.URL.Query().Get("sentence"))
-		result, err := llm.Infer(llm.Llama2Chat, nprompt, nil)
+		// llm infer test
+		// result, err := llm.Infer(llm.Llama2Chat, nprompt, nil)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		// kv test
+		// store, err := kv.OpenDefault()
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		// err = store.Set("testme", []byte("oksure"))
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		// outgoing test
+		// resp, err := spinhttp.Get("https://google.com")
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+		// defer resp.Body.Close()
+
+		// _, err = io.ReadAll(resp.Body)
+		// if err != nil {
+		// 	http.Error(w, "err.Error()", http.StatusInternalServerError)
+		// 	return
+		// }
+
+		_, err := variables.Get("password")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		store, err := kv.OpenDefault()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = store.Set("testme", []byte("oksure"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		resp, err := spinhttp.Get("https://google.com")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		_, err = io.ReadAll(resp.Body)
-		if err != nil {
-			http.Error(w, "err.Error()", http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(resp.StatusCode)
-		w.Write([]byte(result.Text))
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
 	})
 }
 
